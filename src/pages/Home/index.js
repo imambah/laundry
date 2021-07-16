fc2406b7b7c76775a8e23e6d88418b691150aada
imambah <imam.bahtiar@gmail.com>
2021-07-16 22:07:55 +0700

    ubah judul

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,7 +4,7 @@ import {StyleSheet, Text, View} from 'react-native';
 const Home = () => {
   return (
     <View>
-      <Text>Beranda WEB </Text>
+      <Text>Beranda WEB dengan github </Text>
     </View>
   );
 };
